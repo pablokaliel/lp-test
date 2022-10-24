a7de7b6d537eafebaaabcfe4159bfc0dbf913d00
Pablo <pablo.kalyell.441@gmail.com>
2022-10-24 14:54:41 -0200

    clean

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -6,7 +6,7 @@ function Home() {
       <Title>A paz de Deus!</Title>
       <SubTitle>Seja bem-vindo a página do fundo bíblico!</SubTitle>
       <Description>
-      Está página foi desenvolvida com o objetivo de minimizar o processo de visualização dos produtos disponíveis no fundo bíblico(bem como a foto de cada item e valor), para aquisição da cara irmandade.
+      Está página foi desenvolvida com o objetivo de facilitar o processo de visualização dos produtos disponíveis no fundo bíblico(bem como a foto de cada item e valor), para aquisição da cara irmandade.
 É uma página sem fins lucrativos, e, enfatizamos que não são vendas on-line.
  Para adquirir seu produto, deverá comparecer a estante do fundo bíblico e contactar algum irmão (a) responsável.
       </Description>
